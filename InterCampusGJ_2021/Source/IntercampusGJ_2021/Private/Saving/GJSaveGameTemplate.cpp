474c803bef34584c44d444ddc52e4c0732b113a8
DylanJamesRamsden <dylan.james.ramsden@gmail.com>
2021-10-18 15:52:54 +0200

    Repush initial

---
+++ InterCampusGJ_2021/Source/IntercampusGJ_2021/Private/Saving/GJSaveGameTemplate.cpp
@@ -0,0 +1,10 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Saving/GJSaveGameTemplate.h"
+
+UGJSaveGameTemplate::UGJSaveGameTemplate()
+{
+	SaveSlotName = "SaveSlot1";
+	UserIndex = 0;
+}
