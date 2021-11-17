3adbc0eb6995c2123ec2b2936abf9740a018e465
DylanJamesRamsden <dylan.james.ramsden@gmail.com>
2021-11-17 14:07:06 +0200

    Removed RTS demo

--- RTS_Demo/Source/RTS_Demo/RTS_Demo.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "RTS_Demo.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, RTS_Demo, "RTS_Demo" );
--- RTS_Demo/Source/RTS_Demo/RTS_Demo.h
+++
@@ -1,8 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#pragma once
-
-#define ECC_Unit ECC_EngineTraceChannel1
-
-#include "CoreMinimal.h"
-
